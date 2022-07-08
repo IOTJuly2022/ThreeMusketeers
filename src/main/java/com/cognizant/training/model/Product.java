e7a496a8af2744166028b52d9f24255626581e80
William Simpson <william.simpson@cognizant.com>
2022-07-08 12:33:22 -0500

    Implemented Product

--- src/main/java/com/cognizant/training/model/Product.java
+++ src/main/java/com/cognizant/training/model/Product.java
@@ -1,5 +1,38 @@
 package com.cognizant.training.model;
 
-public class Product {
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Getter
+	@Setter
+	@NotNull
+	private String name;
+	
+	@Getter
+	@Setter
+	@Min(0)
+	private Double price;
+	
+	@Getter
+	@Setter
+	@Min(0)
+	@Max(5)
+	private Double rating;
+	
+	public Product(String name, Double price, Double rating) {
+		this.name = name;
+		this.price = price;
+		this.rating = rating;
+	}
 }
