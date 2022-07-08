157d955a42b9365b1bfd7cc6050ccb583a2053dd
William Simpson <william.simpson@cognizant.com>
2022-07-08 11:41:14 -0500

    Initialized project

---
+++ src/test/java/com/cognizant/training/ThreeMusketeersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeMusketeersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
