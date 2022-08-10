d1c61fed41541323455ee24f064a789d265e72da
Wil Simpson <simpson.wil255@gmail.com>
2022-08-10 14:54:25 -0500

    Fixed tests

--- src/test/java/com/cognizant/training/controller/ProductControllerTests.java
+++ src/test/java/com/cognizant/training/controller/ProductControllerTests.java
@@ -2,62 +2,57 @@ package com.cognizant.training.controller;
 
 import com.cognizant.training.model.Product;
 import com.cognizant.training.repository.ProductRepository;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(ProductController.class)
+@ExtendWith(SpringExtension.class)
 public class ProductControllerTests {
 
     @MockBean
     private ProductRepository productRepository;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private MockMvc mockMvc;
 
     private final PodamFactory factory = new PodamFactoryImpl();
 
     @Test
     @WithMockUser
-    public void getById_CanWhenExists() {
+    public void getById_CanWhenExists() throws Exception {
         Product product = factory.manufacturePojo(Product.class);
 
         given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
 
-        ResponseEntity<Product> resp = restTemplate.getForEntity(
-                String.format("/v1/products/%d", product.getId()),
-                Product.class
-        );
-
-        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(resp.getBody()).isNotNull();
-        assertThat(resp.getBody().getId()).isEqualTo(product.getId());
+        mockMvc.perform(get(String.format("/v1/products/%d", product.getId())))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.is(product.getId())));
     }
 
 
     @Test
     @WithMockUser
-    public void getById_ErrorWhenNotExists() {
+    public void getById_ErrorWhenNotExists() throws Exception {
         Long id = factory.manufacturePojo(Long.class);
         given(productRepository.findById(id)).willReturn(Optional.empty());
 
-        ResponseEntity<Product> resp = restTemplate.getForEntity(
-                String.format("/v1/products/%d", id),
-                Product.class
-        );
-
-        assertThat(resp.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
+        mockMvc.perform(get(String.format("/v1/products/%d", id)))
+                .andExpect(status().isBadRequest());
     }
 }
