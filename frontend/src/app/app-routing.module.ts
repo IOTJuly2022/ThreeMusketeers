25b134e47a7330b8ece89b87836a23f7e7dd8599
Andrew Kluttz <William.Kluttz@Cognizant.com>
2022-08-22 14:41:24 -0500

    GUI

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { CatalogComponent } from './pages/catalog/catalog.component';
 import {HomeComponent} from "./pages/home/home.component";
 import {LoginComponent} from "./pages/login/login.component";
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', component: HomeComponent},
+<<<<<<< Updated upstream
   {path: 'login', component: LoginComponent}
+=======
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'catalog', component: CatalogComponent},
+>>>>>>> Stashed changes
 ];
-
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: 'enabled',
+  anchorScrolling: 'enabled',
+  scrollOffset: [0, 64],
+};
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,12 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {JwtInterceptor} from "./_helpers/jwt.interceptor";
 import { FooterComponent } from './_components/footer/footer.component';
+<<<<<<< Updated upstream
+=======
+import { AlertComponent } from './_components/alert/alert.component';
+import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductService } from './_services/catalog/products.service';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -36,7 +42,14 @@ import { FooterComponent } from './_components/footer/footer.component';
     HomeComponent,
     LoginComponent,
     RegisterComponent,
+<<<<<<< Updated upstream
     FooterComponent
+=======
+    FooterComponent,
+    NavbarComponent,
+    AlertComponent,
+    CatalogComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
@@ -66,6 +79,7 @@ import { FooterComponent } from './_components/footer/footer.component';
       useClass: JwtInterceptor,
       multi: true,
     },
+    ProductService
   ],
   bootstrap: [AppComponent]
 })
