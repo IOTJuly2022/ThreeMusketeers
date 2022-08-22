dd5c53071099438f72325a1aa5c1fe336fbabc39
Andrew <snowieraj@gmail.com>
2022-08-22 13:07:07 -0500

    Added function to update quantity

--- frontend/src/app/_services/order.service.ts
+++ frontend/src/app/_services/order.service.ts
@@ -19,15 +19,16 @@ export class OrderService {
     }
     return this.httpClient.get(`${environment.AUTHENTICATION_API_URL}/users/${user.id}/cart`);
   }
-/*
-  setQuantityForSingleProduct(newQuantity : number){
+
+  updateQuantityForSingleProduct(orderDetail : any){
     let user = this.authService.user;
     if(!user) {
       return of();
     }
-    return this.httpClient.set(`${environment.AUTHENTICATION_API_URL}/users/${user.id}/cart`);
+    return this.httpClient.put(`${environment.AUTHENTICATION_API_URL}/users/${user.id}/cart`,{product: orderDetail.product.id,
+                                                                                              count: orderDetail.quantity});
   }
-  */
+
 
 
 }
--- frontend/src/app/pages/cart/cart.component.ts
+++ frontend/src/app/pages/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { OrderService } from '../../_services/order.service'
+import {AlertService} from "../../_services/alert.service";
 
 @Component({
   selector: 'app-cart',
@@ -8,28 +9,36 @@ import { OrderService } from '../../_services/order.service'
 })
 export class CartComponent implements OnInit {
 
-  constructor(private orderService : OrderService) {  }
+  constructor(private orderService : OrderService,
+              private alertService: AlertService) {  }
   quantityCounter : number = 0;
   total : number = 0;
-  availItems : boolean = false  ;
+  availItems : boolean = false;
   orderList : any;
+
   ngOnInit(): void {
     this.orderService.getAllOrdersForCurrentUsers().subscribe((orders : any) => {
       if(!orders) this.availItems = false;
       this.orderList = orders[0];
       this.availItems = true;
-      //this.total = this.orderList.orderDetails[0].product.price;
       this.calculatedTotal(this.orderList);
     });
   }
 
+  //Display service error, not implemented currently
+  checkout(){
+    this.alertService.error('Checkout is not available at this time');
+  }
 
-  updateQuantity(){
+  //Update Quantity of the Product from UI
+  updateQuantity(orderDetail : any){
 
   }
+
+  //Total price calculated for Quantity*Price
   calculatedTotal(orderList : any){
     for(let i = 0; i < orderList.orderDetails.length; i++){
-      this.total += orderList.orderDetails[i].product.price;
+      this.total += orderList.orderDetails[i].product.price * orderList.orderDetails[i].quantity;
     }
   }
 }
